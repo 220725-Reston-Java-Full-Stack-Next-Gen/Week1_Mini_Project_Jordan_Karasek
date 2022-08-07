ff33bae1ecce90c13236ee84bd6ccf289a48cf42
blutrain4 <jordanaustin12@gmail.com>
2022-08-07 00:04:22 -0500

    tweaks

--- src/com/jordan/code/MainDriver.java
+++ src/com/jordan/code/MainDriver.java
@@ -9,11 +9,13 @@ public class MainDriver {
 
 	public static void main(String[] args) {
 		
+		//this is how java makes new objects
 		Thermometer meter = new Thermometer();
 		
 		Scanner scantemp = new Scanner(System.in);
 		System.out.print("Please give your Thermometer a name.");
 		
+		//scantemp.next(); just does string values, scantemp.nextln(); 
 		String name = scantemp.next();
 		meter.setName(name);
 		System.out.println("Hello World, thanks for using " + meter.getName() + " today!");
@@ -30,7 +32,11 @@ public class MainDriver {
 		scantemp.nextLine();
 		meter.setDegreesC(cels);
 
+		//alternate option for calling Thermometer.convertToCelsius
+		//int resultF = obj.convertToFahrenheit(cels);
+		//int resultC = obj.convertToCelsius(fah);
 		
+		//use 
 		System.out.println(fah + " Fahrenheit converted to Celsius is " + Thermometer.convertToCelsius(fah) + " degrees");
 		System.out.println("and");
 		System.out.println(cels + " Celsius converted to Fahrenheit is " + Thermometer.convertToFahrenheit(cels) + " degrees");
@@ -40,17 +46,30 @@ public class MainDriver {
 				for(int i = 0; i < 3; i++) {
 					tempReadings[i] = scantemp.nextInt();
 					
-				}
-				scantemp.close();
+		}
+			scantemp.close();
 				
-				meter.setTempReadings(tempReadings); //has to set the input values in Thermometer class
+		meter.setTempReadings(tempReadings); //has to set the input values in Thermometer class
 				
-				Thermometer.calculateAvgTemp(meter.getTempReadings()); //call the average temp formula and prints out the result.
+		Thermometer.calculateAvgTemp(meter.getTempReadings()); //call the average temp formula and prints out the result.
 				
 				
 		System.out.println("Thanks for using this tool! Goodbye!");
 		
-
+		
+		/*/
+		 * int temp1 = in.nextInt();
+		 * int temp2 = in.nextInt();
+		 * int temp3 = in.nextInt();
+		 * 
+		 * int [] temps = {temp1, temp2, temp3};
+		 * 
+		 * meter.setTempReadings(temps);
+		 * 
+		 * meter.calculateAvgTemp(meter.getTempReadings());
+		 * 
+		 * 
+		 */
 		
 		//[I@19dfb72a
 		//Scanner scan = new Scanner(System.in);
--- src/com/jordan/code/Thermometer.java
+++ src/com/jordan/code/Thermometer.java
@@ -4,23 +4,31 @@ import java.util.Arrays;
 
 public class Thermometer {
 	
+	//class fields
 	private int degreesF;
 	private int degreesC;
 	private String name;
 	
 	private static int[] tempReadings = new int[3]; //integer array with a specified length of 3
 	
-	Thermometer() { //no args constructor?
+	//constructors
+	//these two constructors have the method name but they have a different number....
+	//this concept is known as method overloading
+	//note: method overloading can also occur when there are different datatypes of parameters but the same number parameters
+	public Thermometer() { //no args constructor?
+		super(); //tells the JVM that this is an object and therefore is able..
+		// to override the Object class's method like 
 	}
 	
-	private Thermometer(int degreesF, int degreesC, String name, int[]tempReadings) { // all arg constructor
+	public Thermometer(int degreesF, int degreesC, String name, int[]tempReadings) { // all "arg" constructor
 		this.degreesF = degreesF;
 		this.degreesC = degreesC;
 		this.name = name;
 		this.tempReadings = tempReadings;
 				
 	}
-	
+	//right click go to source >>> Generate Getters and Setters
+	//as we need access to our private class fields here from our Main Driver
 	public int getDegreesF() { //setters and getters for each field 
 		return degreesF;
 	}
@@ -52,12 +60,17 @@ public class Thermometer {
 		this.tempReadings = tempReadings;
 	}
 
+	//right click>>>source >>> Generate toString()
+	//this annotation lets the JVM know that we are overriding the parent class implementation of this method... 
+	//...(aka we are doing our own way to show the Thermometer class details, instead of using Objects class's implementation)
+	//this is called method overriding, which is another form of encapsulation in Java
 	@Override
 	public String toString() {
 		return "Thermometer [degreesF=" + degreesF + ", degreesC=" + degreesC + ", name=" + name + ", tempReadings="
 				+ Arrays.toString(tempReadings) + "]";
 	}
 
+	//Custom method signatures
 	public static int convertToFahrenheit(int c) { //c represents celsius 
 		return (int) (c*(9.0/5.0) + 32); //(int) casts the return value as an integer from the method, then formula for convert
 	}
@@ -67,6 +80,11 @@ public class Thermometer {
 	}
 	
 	public static void calculateAvgTemp(int[] temps) {
+		//need a counter for total temp numbers, starts at 0
+		//iterate thru the array
+		//for each temp, and it to the total counter
+		//end of loop
+		//return the total temps, divided by 3
 		int i, total;
 		int n = 3;
 		total = 0;
@@ -74,6 +92,15 @@ public class Thermometer {
 		for (i = 0; i < n; i++) {
 			total += tempReadings[i];
 		
+			
+			//Azhya example code
+			////cunter for total temp numbers, start at 0
+			//int total = 0;
+			////iterate thru the array
+			//for(int i = 0; i < temps.length; i++) {
+			//	//for each temp, add it to the total counter
+				//total += temps[i];
+			//}
 			
 		} //ran into a lot of trouble because the system out was included in the for loop causing it to print average for each value
 		
